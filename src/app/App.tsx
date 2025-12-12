c763773b74db3a548d762bab433c30b095c940a5
Rafael Santos <96845018+prafaelmsantos@users.noreply.github.com>
2025-12-12 16:16:28 +0000

    remove trial

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,37 +5,18 @@ import AppRouter from "./routes/AppRouter";
 import { LoadingProvider } from "./context/useLoading/LoadingProvider";
 import { ModalProvider } from "./context/useModal/ModalProvider";
 
-const TRIAL_KEY = "trialStart";
-const TRIAL_DAYS = 30;
-const TRIAL_START_DATE = new Date("2025-11-20T00:00:00Z");
-
 function App() {
-  let start = localStorage.getItem(TRIAL_KEY);
-  if (!start) {
-    start = TRIAL_START_DATE.toISOString();
-    localStorage.setItem(TRIAL_KEY, start);
-  }
-
-  const startDate = new Date(start);
-  const diffDays = Math.floor(
-    (new Date().getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)
-  );
-
-  const isExpired: boolean = diffDays >= TRIAL_DAYS;
   return (
     <AppTheme>
       <CssBaseline enableColorScheme />
-      {isExpired ? (
-        "Seu período de teste expirou."
-      ) : (
-        <NotificationsProvider>
-          <LoadingProvider>
-            <ModalProvider>
-              <AppRouter />
-            </ModalProvider>
-          </LoadingProvider>
-        </NotificationsProvider>
-      )}
+
+      <NotificationsProvider>
+        <LoadingProvider>
+          <ModalProvider>
+            <AppRouter />
+          </ModalProvider>
+        </LoadingProvider>
+      </NotificationsProvider>
     </AppTheme>
   );
 }
